416828ca30ce408bed2cb6e4c028133fdb7a92bb
Victor Hang <vhvictorhang@gmail.com>
2025-01-03 21:03:02 +0100

    fix 🐛: revert c74a33a
    Signed-off-by: Victor Hang <vhvictorhang@gmail.com>

--- internal/utils/git.go
+++ internal/utils/git.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"syscall"
 
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
@@ -72,15 +71,7 @@ func RunCommand(dirName, command string, args ...string) error {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Dir = dirName
-	// Ensure the command runs independently of the parent process
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Setpgid: true, // Start the command in a new process group
-	}
-	// Start the command
-	if err := cmd.Start(); err != nil {
-		return err
-	}
-	return cmd.Wait()
+	return cmd.Run()
 }
 
 func GetLatestRemoteCommit(r *git.Repository, branch string) (string, error) {
